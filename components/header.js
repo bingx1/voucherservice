e1184eac959724630c5c2a980040bd41a5d39a44
bing xu <bingxu.au@gmail.com>
2021-04-30 00:15:17 +1000

    added header and signup page

--- components/header.js
+++ components/header.js
@@ -6,16 +6,31 @@ import Typography from '@material-ui/core/Typography';
 import Button from '@material-ui/core/Button';
 import IconButton from '@material-ui/core/IconButton';
 import MenuIcon from '@material-ui/icons/Menu';
+import Link from 'next/link';
+import LoyaltyIcon from '@material-ui/icons/Loyalty';
+import NavButton from './NavButton';
 
 const useStyles = makeStyles((theme) => ({
   root: {
-    flexGrow: 1,
+    // flexGrow: 1,
+    width: '100%',
+    zIndex: 100,
+    padding: 0,
   },
   menuButton: {
     marginRight: theme.spacing(2),
+    // paddingRight:
   },
   title: {
     flexGrow: 1,
+    fontFamily: 'Poppins',
+    fontSize: 18,
+    fontWeight: 600,
+    paddingLeft: '10px'
+  },
+  logoIcon: {
+    flexGrow: 1,
+    paddingLeft: '0px',
   },
 }));
 
@@ -29,10 +44,16 @@ export default function Header() {
           <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
             <MenuIcon />
           </IconButton>
-          <Typography variant="h6" className={classes.title}>
-            Voucher Service
-          </Typography>
-          <Button color="inherit">Login</Button>
+          <Link href='/'>
+              <a className={classes.logoIcon}>
+                <IconButton>
+                    <LoyaltyIcon></LoyaltyIcon>
+                    <Typography variant="h6" className={classes.title}> Voucher Service </Typography>
+                </IconButton>
+              </a>
+          </Link>
+          <NavButton url='/signup' text='Sign up' color="inherit"/>
+          <NavButton url='/login' text='Login' color="inherit"/>
         </Toolbar>
       </AppBar>
     </div>
